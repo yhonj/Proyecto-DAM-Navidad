3f7acd1e822a3f4cf99bb6da3178bc61534336a2
Yhon <ionmalto@gmail.com>
2026-01-04 16:22:13 +0100

    Initial project structure

---
+++ src/main/java/com/yhon/dam/proyectodamnavidad/ProyectoDAMNavidad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yhon.dam.proyectodamnavidad;
+
+/**
+ *
+ * @author yhon
+ */
+public class ProyectoDAMNavidad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
